34dd7c65b698cc807e4d8a63d5222896bc493bc0
“Zhnagying <l>
2018-12-21 09:36:43 +0800

    add files

---
+++ newest/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad:function(){
+			
+		},
+		methods: {
+			enterFilm(e){
+				var newsid = e.currentTarget.dataset.newid;
+				uni.navigateTo({
+					url: '../info/info?newid='+newsid,
+					success: res => {},
+					fail: () => {},
+					complete: () => {}
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+		
+	
+	.uni-media-list-body{
+		height:auto;
+	}
+		
+	.uni-media-list-text-top{
+		line-height: 1.6em;
+	}
+</style>
---
+++ newest/pages/info/info.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content">
+		<view class="text">{{title}}</view>
+		<view class="art-contant">
+			<rich-text class="richText" :nodes="node"></rich-text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title:'',
+				node:'',
+				
+			};
+		},
+		onLoad:function(e){
+			console.log(e)
+			uni.showLoading({
+				title:'加载中....'
+			})
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/'+e.newid,
+				method: 'GET',
+				data: {},
+				success: res => {
+					
+					uni.hideLoading();
+					this.title = res.data.title;
+					this.node = res.data.content;
+					console.log(res)
+					
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		}
+	}
+</script>
+
+<style>
+	
+.content{
+	padding:10upx 2%;
+	width:96%;
+	flex-wrap: wrap;
+}
+.text{
+	line-height: 2em;
+	font-weight: 700;
+	font-size: 38upx;
+}
+.art-contant{
+	line-height: 2em;
+}
+</style>
